98d49341ec7fa0fc8716f8c69c59e48802d041f3
Furkan Gülhan <fgulhan07@gmail.com>
2024-10-31 15:22:13 +0300

    Update json
    Fills study_suggestion by using AI
    Checking if student's answer is correct
    Supports multiple questions % students

--- GenAI/main.py
+++ GenAI/main.py
@@ -35,6 +35,34 @@ for question in data['test']['questions']:
     # Update the question label
     question['question_label'] = find_question_label(response.text)
 
+wrong_labels = []
+
+for student in data['students']:
+    wrong_labels.clear()
+    for response in student['responses']:
+        q_id : int = response['question_id']
+
+        # Find the question with the same id
+        question_info = next((q for q in data['test']['questions'] if q['question_id'] == q_id), None)
+        correct_answer = question_info['correct_answer']
+
+        if response['selected_option'] == correct_answer:
+            response['is_correct'] = True
+        else:
+            response['is_correct'] = False
+            question_label = question_info['question_label']
+            wrong_labels.append(question_label)
+
+    print("suggestions: " + 55*"-")
+
+    print("Suggest a study topic based on the following question labels: " + ', '.join(wrong_labels))
+    suggestion_AI = model.generate_content("Suggest a one-sentence study recommendation for an elementary school child who is making mistakes in the following subjects: " + ', '.join(wrong_labels))
+    print(suggestion_AI.text)
+    student['study_suggestion'] = suggestion_AI.text
+
+
+
+
 # Save the updated json file
 with open('updated_json.json', 'w') as f:
     json.dump(data, f, indent=1)
